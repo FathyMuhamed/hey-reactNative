e8d49ced8edbfc39f2633f119c588356aa89cc6f
FathyMuhamed <fathimuhamed10522@gmail.com>
2024-12-08 20:28:12 +0200

    Refactor PrayerCard component to use StyleSheet for improved readability and maintainability; streamline inline styles into a dedicated styles object

--- components/PrayerCard.tsx
+++ components/PrayerCard.tsx
@@ -1,55 +1,48 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { Colors } from "../constants/Colors";
 
 export default function PrayerCard() {
   return (
-    <View
-      style={{
-        backgroundColor: "red",
-        borderRadius: 12,
-        marginVertical: 16,
-        height: 200,
-        width: "100%",
-        position: "relative",
-      }}
-    >
+    <View style={styles.container}>
       <Image
-        style={{
-          width: "100%",
-          height: "100%",
-          borderRadius: 12,
-          objectFit: "cover",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+        style={styles.image}
         source={{
           uri: "https://placehold.co/200/546518/ffffff/png",
           scale: 1,
         }}
       />
-      <View
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "5%",
-        }}
-      >
-        <Text
-          style={{
-            color: Colors.light.white,
-          }}
-        >
-          PrayerCard
-        </Text>
-        <Text
-          style={{
-            color: Colors.light.white,
-          }}
-        >
-          PrayerCard
-        </Text>
+      <View style={styles.overlay}>
+        <Text style={styles.text}>PrayerCard</Text>
+        <Text style={styles.text}>PrayerCard</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "red",
+    borderRadius: 12,
+    marginVertical: 16,
+    height: 200,
+    width: "100%",
+    position: "relative",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 12,
+    objectFit: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlay: {
+    position: "absolute",
+    top: "50%",
+    left: "5%",
+  },
+  text: {
+    color: Colors.light.white,
+  },
+});
